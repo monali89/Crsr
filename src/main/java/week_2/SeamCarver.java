5a1e023b2d03150f4ab9eabfb9ab8d4b38be72a7
Monali Lakhote <monalidl@gmail.com>
2020-03-07 19:47:27 -0500

    Working for given starting point in column - verticalSeam

--- src/main/java/week_2/SeamCarver.java
+++ src/main/java/week_2/SeamCarver.java
@@ -1,8 +1,9 @@
 package week_2;
 
-import edu.princeton.cs.algs4.Digraph;
-import edu.princeton.cs.algs4.IndexMinPQ;
 import edu.princeton.cs.algs4.Picture;
+
+import java.awt.*;
+import java.util.Random;
 
 /**
  * @author Monali L on 2/29/2020
@@ -10,21 +11,22 @@ import edu.princeton.cs.algs4.Picture;
 
 public class SeamCarver {
 
-    class Pixel {
+    /*class Pixel {
         int x;
         int y;
         double energy;
-    }
+    }*/
 
     private Picture picture;
-    private Digraph dg;
+    //private Digraph dg;
     private int size;
 
     // create a seam carver object based on the given picture
     public SeamCarver(Picture picture) {
         if (picture == null) throw new IllegalArgumentException();
-
         this.picture = picture;
+
+        /*this.picture = picture;
         this.size = picture.height() * picture.width();
         this.dg = new Digraph(size);
         System.out.println("DEBUG: Created graph of size - " + size);
@@ -53,15 +55,15 @@ public class SeamCarver {
             }
         }
 
-        /*Topological topological = new Topological(dg);
+        *//*Topological topological = new Topological(dg);
         System.out.println("DEBUG: T hasOrder - " + topological.hasOrder());
-        System.out.println("DEBUG: T Order - " + topological.order());*/
-        System.out.println("DEBUG: dg - " + dg.toString());
+        System.out.println("DEBUG: T Order - " + topological.order());*//*
+        System.out.println("DEBUG: dg - " + dg.toString());*/
     }
 
-    private int getIndex(int x, int y) {
-        int index = x * picture.width() + y;
-        System.out.println("DEBUG: Getting Index (" + x + "," + y + ") - " + index);
+    private int getIndex(int x, int y) { // Column x, row y
+        int index = y * picture.width() + x;
+        System.out.println("DEBUG: Getting Index (Col, Row) -> (" + x + "," + y + ") - " + index);
         return index;
     }
 
@@ -82,27 +84,27 @@ public class SeamCarver {
 
     // energy of pixel at column x and row y
     public double energy(int x, int y) {
-        if (x <0 || y < 0 || x > picture.width() || y > picture.height())
+        if (x <0 || y < 0 || x >= picture.width() || y >= picture.height())
             throw new IllegalArgumentException();
 
         // Border coordinate
-        if(x == 0 || x == picture.height()-1 || y == 0 || y == picture.width()-1)
+        if(x == 0 || x == picture.width()-1 || y == 0 || y == picture.height()-1)
             return 1000.0;
 
         // X gradient
-        int x_red = picture.get(y, x+1).getRed() - picture.get(y, x-1).getRed();
-        int x_blue = picture.get(y, x+1).getBlue() - picture.get(y, x-1).getBlue();
-        int x_green = picture.get(y, x+1).getGreen() - picture.get(y, x-1).getGreen();
+        int x_red = picture.get(x+1, y).getRed() - picture.get(x-1, y).getRed();
+        int x_blue = picture.get(x+1, y).getBlue() - picture.get(x-1, y).getBlue();
+        int x_green = picture.get(x+1, y).getGreen() - picture.get(x-1, y).getGreen();
         int x_sqr = x_red*x_red + x_blue*x_blue + x_green*x_green;
 
         // Y gradient
-        int y_red = picture.get(y+1, x).getRed() - picture.get(y-1, x).getRed();
-        int y_blue = picture.get(y+1, x).getBlue() - picture.get(y-1, x).getBlue();
-        int y_green = picture.get(y+1, x).getGreen() - picture.get(y-1, x).getGreen();
+        int y_red = picture.get(x, y+1).getRed() - picture.get(x, y-1).getRed();
+        int y_blue = picture.get(x, y+1).getBlue() - picture.get(x, y-1).getBlue();
+        int y_green = picture.get(x, y+1).getGreen() - picture.get(x, y-1).getGreen();
         int y_sqr = y_red*y_red + y_blue*y_blue + y_green*y_green;
 
         double energy = Math.sqrt(x_sqr + y_sqr);
-        System.out.println("DEBUG: Energy for pixel + (" + x + "," + y + ") - " + energy);
+        //System.out.println("DEBUG: Energy for pixel (Col, Row) -> (" + x + "," + y + ") - " + energy);
 
         return energy;
     }
@@ -115,64 +117,37 @@ public class SeamCarver {
     // sequence of indices for vertical seam
     public int[] findVerticalSeam() {
 
-        int[] arr = new int[height()];
-        double[] distTo = new double[height()];
-        int[] edgeTo = new int[height()];
-
-        int y = 2;
-        distTo[0] = 0;
-        edgeTo[0] = -1;
-
-        IndexMinPQ<Double> pq = new IndexMinPQ<Double>(height());
-        pq.insert(0, energy(0, 2));
-
-        for (int x = 0; x < height()-1; x++) {
-            // left
-            double left_e = energy(x+1, y-1);
-            // center
-            double center_e = energy(x+1, y);
-            // right
-            double right_e = energy(x+1, y+1);
-
-        }
-
         return null;
     }
 
     public void verticalSeamHelper(int start) {
+
         double[] distTo = new double[height()];
-        int[] prev = new int[height()];
-        int y = start;
-        prev[0] = start;
+        int[] edgeTo = new int[height()];
         distTo[0] = 1000.0;
-        for (int x = 0; x < height()-1; x++) {
-            double l_e = 0.0, c_e = 0.0, r_e = 0.0;
-            if (y-1 > 0) l_e = energy(x+1, y-1);
-            c_e = energy(x+1, y);
-            if (y+1 < width()) r_e = energy(x+1, y+1);
-            if (l_e <= c_e) {
-                if (l_e <= r_e) { // left is min
-                    distTo[x+1] = distTo[x] + l_e;
-                    prev[x+1] = y-1;
-                } else { // right is min
-                    distTo[x+1] = distTo[x] + r_e;
-                    prev[x+1] = y+1;
-                }
-            } else {
-                if (c_e <= r_e) { // center is min
-                    distTo[x+1] = distTo[x] + c_e;
-                    prev[x+1] = y;
-                } else { // right is min
-                    distTo[x+1] = distTo[x] + r_e;
-                    prev[x+1] = y+1;
+        edgeTo[0] = start;
+
+        int x = start;
+
+        for (int y = 0; y < height()-1; y++) {
+            double current_dist = distTo[y];
+            double min_energy = Double.POSITIVE_INFINITY;
+            for (int i = -1; i <= 1; i++) {
+                if (x + i*1 < -1 || x + i*1 >= width()) continue;
+                double this_energy = energy(x + i*1, y+1);
+                if (this_energy < min_energy) {
+                    distTo[y+1] = distTo[y] + this_energy;
+                    edgeTo[y+1] = x + i*1;
+                    min_energy = this_energy;
                 }
             }
-            y = prev[x+1];
+            x = edgeTo[y+1];
         }
+
         System.out.println("DEBUG: Final distance - " + distTo[height()-1]);
         System.out.println("DEBUG: Edges - ");
-        for (int i = 0; i < prev.length; i++) {
-            System.out.println("Prev[" + i + "] - " + prev[i]);
+        for (int i = 0; i < edgeTo.length; i++) {
+            System.out.println("Edge[" + i + "] - " + edgeTo[i]);
         }
     }
 
@@ -192,11 +167,43 @@ public class SeamCarver {
     //  unit testing (optional)
     public static void main(String[] args) {
 
-        Picture picture = new Picture(4,3);
+        Picture picture = new Picture("C:\\Users\\monal\\IdeaProjects\\Coursera\\src\\main\\resources\\week_2\\6x5.png");
         SeamCarver seamCarver = new SeamCarver(picture);
-        seamCarver.verticalSeamHelper(3);
-        //Topological topological = new Topological();
 
+        System.out.println("DEBUG: Energy matrix");
+        for (int i = 0; i < seamCarver.height(); i++) {
+            for (int j = 0; j < seamCarver.width(); j++) {
+                System.out.print(Math.round(seamCarver.energy(j, i)) + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        seamCarver.verticalSeamHelper(3);
     }
+
+    private static Picture getPictureWxH(int width, int height) {
+
+        Picture picture = new Picture(width, height);
+        Color color;
+
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                int r = getRandomBetween(0, 255);
+                int g = getRandomBetween(0, 255);
+                int b = getRandomBetween(0, 255);
+                color = new Color(r, g, b);
+                picture.setRGB(i, j, color.getRGB());
+            }
+        }
+        return picture;
+    }
+
+    private static int getRandomBetween(int min, int max) {
+        Random r = new Random();
+        int random = r.nextInt(max+1-min) + min;
+        return random;
+    }
+
 
 }
