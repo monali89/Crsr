66cf7fcc31ba5b68b02a00dd71207bf7e58bc4c2
Monali Lakhote <monalidl@gmail.com>
2020-04-15 18:05:28 -0400

    Updated maps, queue to 2D arrays

--- src/main/java/week_2/SeamCarver.java
+++ src/main/java/week_2/SeamCarver.java
@@ -1,10 +1,6 @@
 package week_2;
 
-import edu.princeton.cs.algs4.IndexMinPQ;
 import edu.princeton.cs.algs4.Picture;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author Monali L on 2/29/2020
@@ -12,18 +8,20 @@ import java.util.Map;
 
 public class SeamCarver {
 
-    private Picture picture;
     private static final double BORDER_ENERGY = 1000.0;
+    private Picture picture;
+    private int[][] yx;
 
     // create a seam carver object based on the given picture
     public SeamCarver(Picture picture) {
         if (picture == null) throw new IllegalArgumentException();
-        this.picture = picture;
+        this.picture = new Picture(picture);
+        this.yx = new int[height()*width()][2];
     }
 
     // current picture
     public Picture picture() {
-        Picture newPicture = this.picture;
+        Picture newPicture = new Picture(this.picture);
         return newPicture;
     }
 
@@ -68,64 +66,34 @@ public class SeamCarver {
 
         double[][] distTo = new double[height()][width()];
         int[][] edgeTo = new int[height()][width()];
+        boolean[][] isVisited = new boolean[height()][width()];
         int[] minArray = new int[width()];
-        IndexMinPQ<Double> pq = new IndexMinPQ<Double>(width()*height());
 
-        Map<Integer, int[]> indexMap = new HashMap<Integer, int[]>();
-        for (int i = 0; i < height(); i++) { // rows
-            for (int j = 0; j < width(); j++) { // columns
-                indexMap.put(getIndex(j, i), new int[]{i, j});
-                distTo[i][j] = Double.POSITIVE_INFINITY;
+        for (int i = 0; i < height(); i++) {
+            for (int j = 0; j < width(); j++) {
+                distTo[i][j] = j == 0 ? BORDER_ENERGY : Double.POSITIVE_INFINITY;
             }
-            distTo[i][0] = BORDER_ENERGY;
-            pq.insert(getIndex(0, i), BORDER_ENERGY);
         }
 
-        while (!pq.isEmpty()) {
+        for (int j = 0; j < height()*width(); j++) {
 
-            int current = pq.delMin();
-            int[] arr = indexMap.get(current);
-            int y = arr[0]; // col
-            int x = arr[1]; // row
-
+            int current = getMinDistIndex(distTo, isVisited);
+            int y = yx[current][0]; // col
+            int x = yx[current][1]; // row
             if (x >= width()-1) break;
 
+            isVisited[y][x] = true;
+
+            // because everything here is width x height (cols x rows)
             for (int i = -1; i <= 1; i++) {
-
                 if (y + i <= -1 || y + i >= height()) continue;
-                int adjIndex = getIndex(x + 1, y + i);
                 double dist = energy(x + 1, y + i) + distTo[y][x];
-
-                if (distTo[y+i][x+1] > dist) {
+                if (!isVisited[y+i][x+1] && distTo[y+i][x+1] > dist) {
                     distTo[y+i][x+1] = dist;
                     edgeTo[y+i][x+1] = current;
-
-                    if (pq.contains(current)) {
-                        pq.decreaseKey(adjIndex, distTo[y+i][x+1]);
-                    } else {
-                        pq.insert(adjIndex, distTo[y+i][x+1]);
-                    }
                 }
             }
         }
-
-        System.out.println("DistTo");
-        for (int i = 0; i < height(); i++) {
-            for (int j = 0; j < width(); j++) {
-                System.out.print(Math.round(distTo[i][j]) + " ");
-            }
-            System.out.println();
-        }
-        System.out.println();
-
-        System.out.println("EdgeTo");
-        for (int i = 0; i < height(); i++) {
-            for (int j = 0; j < width(); j++) {
-                System.out.print(edgeTo[i][j] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println();
 
         // Get min array
         double minEnergy = Double.POSITIVE_INFINITY;
@@ -133,20 +101,14 @@ public class SeamCarver {
         for (int i = 0; i < height(); i++) {
             if (distTo[i][width()-1] < minEnergy) {
                 minEnergy = distTo[i][width()-1];
-                minIndex = edgeTo[i][width()-1];
+                minIndex = getIndex(width()-1, i);
             }
         }
 
-        for (int i = height()-1; i > -1; i--) {
-            int[] arr = indexMap.get(minIndex);
-            int y = arr[0]; // col
-            int x = arr[1]; // row
+        for (int i = width()-1; i > -1; i--) {
+            int y = yx[minIndex][0]; // col
             minArray[i] = y;
             minIndex = edgeTo[y][i];
-        }
-
-        for (int i = 0; i < minArray.length; i++) {
-            System.out.println("DEBUG: MinArray " + i + " - " + minArray[i] + ", Index - " + edgeTo[minArray[i]][i] + ", Dist - " + distTo[minArray[i]][i]);
         }
 
         return minArray;
@@ -159,61 +121,34 @@ public class SeamCarver {
 
         double[][] distTo = new double[height()][width()];
         int[][] edgeTo = new int[height()][width()];
+        boolean[][] isVisited = new boolean[height()][width()];
         int[] minArray = new int[height()];
-        IndexMinPQ<Double> pq = new IndexMinPQ<Double>(width()*height());
 
-        Map<Integer, int[]> indexMap = new HashMap<Integer, int[]>();
         for (int i = 0; i < height(); i++) {
             for (int j = 0; j < width(); j++) {
-                indexMap.put(getIndex(j, i), new int[]{i, j}); // col, row
-                distTo[i][j] = Double.POSITIVE_INFINITY;
+                distTo[i][j] = i == 0 ? BORDER_ENERGY : Double.POSITIVE_INFINITY;
             }
         }
-        for (int i = 0; i < width(); i++) {
-            distTo[0][i] = BORDER_ENERGY;
-            pq.insert(getIndex(i, 0), distTo[0][i]);
-        }
 
-        while (!pq.isEmpty()) {
-            int current = pq.delMin();
-            int[] arr = indexMap.get(current);
-            int y = arr[0]; // col
-            int x = arr[1]; // row
-            if (y >= height()-1) break;
+        for (int j = 0; j < height()*width(); j++) {
+
+            int current = getMinDistIndex(distTo, isVisited);
+            int y = yx[current][0]; // col
+            int x = yx[current][1]; // row
+            if (y >= height()-1) continue;
+
+            isVisited[y][x] = true;
+
             // because everything here is width x height (cols x rows)
             for (int i = -1; i <= 1; i++) {
                 if (x + i <= -1 || x + i >= width()) continue;
-                int adjIndex = getIndex(x+i, y+1);
                 double dist = energy(x + i, y + 1) + distTo[y][x];
-                if (distTo[y+1][x+i] > dist) {
+                if (!isVisited[y+1][x+i] && distTo[y+1][x+i] > dist) {
                     distTo[y+1][x+i] = dist;
                     edgeTo[y+1][x+i] = current;
-                    if (pq.contains(current)) {
-                        pq.decreaseKey(adjIndex, distTo[y+1][x+i]);
-                    } else {
-                        pq.insert(adjIndex, distTo[y+1][x+i]);
-                    }
                 }
             }
         }
-
-        System.out.println("DistTo");
-        for (int i = 0; i < height(); i++) {
-            for (int j = 0; j < width(); j++) {
-                System.out.print(Math.round(distTo[i][j]) + " ");
-            }
-            System.out.println();
-        }
-        System.out.println();
-
-        System.out.println("EdgeTo");
-        for (int i = 0; i < height(); i++) {
-            for (int j = 0; j < width(); j++) {
-                System.out.print(edgeTo[i][j] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println();
 
         // Get min array
         double minEnergy = Double.POSITIVE_INFINITY;
@@ -221,67 +156,80 @@ public class SeamCarver {
         for (int i = 0; i < width(); i++) {
             if (distTo[height()-1][i] < minEnergy) {
                 minEnergy = distTo[height()-1][i];
-                minIndex = edgeTo[height()-1][i];
+                minIndex = getIndex(i, height()-1);
             }
         }
 
         for (int i = height()-1; i > -1; i--) {
-            int[] arr = indexMap.get(minIndex);
-            int y = arr[0]; // col
-            int x = arr[1]; // row
+            int x = yx[minIndex][1]; // row
             minArray[i] = x;
             minIndex = edgeTo[i][x];
-        }
-
-        for (int i = 0; i < minArray.length; i++) {
-            System.out.println("DEBUG: MinArray " + i + " - " + minArray[i] + ", Index - " + edgeTo[i][minArray[i]] + ", Dist - " + distTo[i][minArray[i]]);
         }
 
         return minArray;
     }
 
-    public int getIndex(int width, int height) {
-        return height*width() + width;
+    private int getMinDistIndex(double[][] distTo, boolean[][] isVisited) {
+
+        double minDist = Double.POSITIVE_INFINITY;
+        int minIndex = -1;
+
+        for (int i = 0; i < height(); i++) {
+            for (int j = 0; j < width(); j++) {
+                if (!isVisited[i][j] && distTo[i][j] < minDist) {
+                    minDist = distTo[i][j];
+                    minIndex = getIndex(j, i);
+                }
+            }
+        }
+        return minIndex;
+    }
+
+    private int getIndex(int width, int height) {
+        int ind = height*width() + width;
+        yx[ind][0] = height;
+        yx[ind][1] = width;
+        return ind;
     }
 
     // remove horizontal seam from current picture
     public void removeHorizontalSeam(int[] seam) {
 
-        if (seam == null || seam.length > picture.width() || picture.height() <= 1 || !isSeamValid(seam))
+        if (seam == null || seam.length >= picture.width() || picture.height() <= 1 || !isSeamValid(seam))
             throw new IllegalArgumentException();
 
-        Picture mod_pic = new Picture(width(), height()-1);
+        Picture modPic = new Picture(width(), height()-1);
         int seamIndex = 0;
 
         for (int x = 0; x < width(); x++) {
             int modPicIndex = 0;
             for (int y = 0; y < height(); y++) {
                 if (y == seam[seamIndex]) continue;
-                mod_pic.setRGB(x, modPicIndex++, picture.getRGB(x, y));
+                modPic.setRGB(x, modPicIndex++, picture.getRGB(x, y));
             }
             seamIndex++;
         }
-        picture = mod_pic;
+        picture = modPic;
     }
 
     // remove vertical seam from current picture
     public void removeVerticalSeam(int[] seam) {
 
-        if (seam == null || seam.length > picture.height() || picture.width() <= 1 || !isSeamValid(seam))
+        if (seam == null || seam.length >= picture.height() || picture.width() <= 1 || !isSeamValid(seam))
             throw new IllegalArgumentException();
 
-        Picture mod_pic = new Picture(width()-1, height());
+        Picture modPic = new Picture(width()-1, height());
         int seamIndex = 0;
 
         for (int y = 0; y < height(); y++) {
             int modPicIndex = 0;
             for (int x = 0; x < width(); x++) {
                 if (x == seam[seamIndex]) continue;
-                mod_pic.setRGB(modPicIndex++, y, picture.getRGB(x, y));
+                modPic.setRGB(modPicIndex++, y, picture.getRGB(x, y));
             }
             seamIndex++;
         }
-        picture = mod_pic;
+        picture = modPic;
     }
 
     private boolean isSeamValid(int[] seam) {
