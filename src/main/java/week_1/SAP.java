f4844845883656bb540dd9e6cde7acde3acae3c1
Monali Lakhote <monalidl@gmail.com>
2020-05-21 16:19:50 -0400

    code refactoring

--- src/main/java/week_1/SAP.java
+++ src/main/java/week_1/SAP.java
@@ -1,16 +1,22 @@
 package week_1;
 
-import edu.princeton.cs.algs4.*;
+import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
+
 
 /**
  * @author Monali L on 2/8/2020
  */
 public class SAP {
 
-    private Digraph dg;
+    private final Digraph dg;
 
     // constructor takes a digraph (not necessarily a DAG)
     public SAP(Digraph G) {
@@ -19,182 +25,38 @@ public class SAP {
 
     // length of shortest ancestral path between v and w; -1 if no such path
     public int length(int v, int w) {
-
-        Set<Integer> vSet = new HashSet<Integer>();
-        Set<Integer> wSet = new HashSet<Integer>();
-
-        Queue<Integer> vQueue = new Queue<Integer>();
-        Queue<Integer> wQueue = new Queue<Integer>();
-
-        vQueue.enqueue(v);
-        wQueue.enqueue(w);
-
-        boolean[] vIsVisited = new boolean[dg.V()];
-        boolean[] wIsVisited = new boolean[dg.V()];
-
-        int[] vDistTo = new int[dg.V()];
-        int[] wDistTo = new int[dg.V()];
-
-        while (!vQueue.isEmpty() || !wQueue.isEmpty()) {
-
-            int vCurr = -1, wCurr = -1, vDist = 0, wDist = 0;
-            if (!vQueue.isEmpty()) {
-                vCurr = vQueue.dequeue();
-                vDist = vDistTo[vCurr];
-            }
-            if (!wQueue.isEmpty()) {
-                wCurr = wQueue.dequeue();
-                wDist = wDistTo[wCurr];
-            }
-
-            if (vSet.contains(wCurr)) {
-                return vDistTo[wCurr] + wDistTo[wCurr];
-            } else if (wSet.contains(vCurr)) {
-                return vDistTo[vCurr] + wDistTo[vCurr];
-            }
-
-            if (vCurr != -1) {
-                for (int adj: dg.adj(vCurr)) {
-                    if (!vIsVisited[adj]) {
-                        vSet.add(adj);
-                        vQueue.enqueue(adj);
-                        vIsVisited[adj] = true;
-                        if (vDistTo[adj] < vDist + 1) {
-                            vDistTo[adj] = vDist + 1;
-                        }
-                    }
-                }
-            }
-            if (wCurr != -1) {
-                for (int adj: dg.adj(wCurr)) {
-                    if (!wIsVisited[adj]) {
-                        wSet.add(adj);
-                        wQueue.enqueue(adj);
-                        wIsVisited[adj] = true;
-                        if (wDistTo[adj] < wDist + 1) {
-                            wDistTo[adj] = wDist + 1;
-                        }
-                    }
-                }
-            }
-        }
-        return -1;
+        if (v < 0 || w < 0 || v >= dg.V() || w >= dg.V()) throw  new IllegalArgumentException();
+        return helperLength(Collections.singletonList(v), Collections.singletonList(w));
     }
 
     // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
     public int ancestor(int v, int w) {
-
-        Set<Integer> vSet = new HashSet<Integer>();
-        Set<Integer> wSet = new HashSet<Integer>();
-
-        Queue<Integer> vQueue = new Queue<Integer>();
-        Queue<Integer> wQueue = new Queue<Integer>();
-
-        vQueue.enqueue(v);
-        wQueue.enqueue(w);
-
-        boolean[] vIsVisited = new boolean[dg.V()];
-        boolean[] wIsVisited = new boolean[dg.V()];
-
-        while (!vQueue.isEmpty() || !wQueue.isEmpty()) {
-
-            int vCurr = vQueue.isEmpty() ? -1 : vQueue.dequeue();
-            int wCurr = wQueue.isEmpty() ? -1 : wQueue.dequeue();
-
-            if (vSet.contains(wCurr)) {
-                return wCurr;
-            } else if (wSet.contains(vCurr)) {
-                return vCurr;
-            }
-
-            if (vCurr != -1) {
-                for (int adj: dg.adj(vCurr)) {
-                    if (!vIsVisited[adj]) {
-                        vSet.add(adj);
-                        vQueue.enqueue(adj);
-                        vIsVisited[adj] = true;
-                    }
-                }
-            }
-            if (wCurr != -1) {
-                for (int adj: dg.adj(wCurr)) {
-                    if (!wIsVisited[adj]) {
-                        wSet.add(adj);
-                        wQueue.enqueue(adj);
-                        wIsVisited[adj] = true;
-                    }
-                }
-            }
-        }
-        return -1;
+        if (v < 0 || w < 0 || v >= dg.V() || w >= dg.V()) throw  new IllegalArgumentException();
+        return helperAncestor(Collections.singletonList(v), Collections.singletonList(w));
     }
 
     // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
     public int length(Iterable<Integer> v, Iterable<Integer> w) {
 
-        Set<Integer> vSet = new HashSet<Integer>();
-        Set<Integer> wSet = new HashSet<Integer>();
+        if (v == null || w == null) throw new IllegalArgumentException();
+        for (int vThis: v) if (vThis < 0 || vThis >= dg.V()) throw new IllegalArgumentException();
+        for (int wThis: w) if (wThis < 0 || wThis >= dg.V()) throw new IllegalArgumentException();
 
-        Queue<Integer> vQueue = new Queue<Integer>();
-        Queue<Integer> wQueue = new Queue<Integer>();
-
-        for (int i: v) vQueue.enqueue(i);
-        for (int i: w) wQueue.enqueue(i);
-
-        boolean[] vIsVisited = new boolean[dg.V()];
-        boolean[] wIsVisited = new boolean[dg.V()];
-
-        int[] vDistTo = new int[dg.V()];
-        int[] wDistTo = new int[dg.V()];
-
-        while (!vQueue.isEmpty() || !wQueue.isEmpty()) {
-
-            int vCurr = -1, wCurr = -1, vDist = 0, wDist = 0;
-            if (!vQueue.isEmpty()) {
-                vCurr = vQueue.dequeue();
-                vDist = vDistTo[vCurr];
-            }
-            if (!wQueue.isEmpty()) {
-                wCurr = wQueue.dequeue();
-                wDist = wDistTo[wCurr];
-            }
-
-            if (vSet.contains(wCurr)) {
-                return vDistTo[wCurr] + wDistTo[wCurr];
-            } else if (wSet.contains(vCurr)) {
-                return vDistTo[vCurr] + wDistTo[vCurr];
-            }
-
-            if (vCurr != -1) {
-                for (int adj: dg.adj(vCurr)) {
-                    if (!vIsVisited[adj]) {
-                        vSet.add(adj);
-                        vQueue.enqueue(adj);
-                        vIsVisited[adj] = true;
-                        if (vDistTo[adj] < vDist + 1) {
-                            vDistTo[adj] = vDist + 1;
-                        }
-                    }
-                }
-            }
-            if (wCurr != -1) {
-                for (int adj: dg.adj(wCurr)) {
-                    if (!wIsVisited[adj]) {
-                        wSet.add(adj);
-                        wQueue.enqueue(adj);
-                        wIsVisited[adj] = true;
-                        if (wDistTo[adj] < wDist + 1) {
-                            wDistTo[adj] = wDist + 1;
-                        }
-                    }
-                }
-            }
-        }
-        return -1;
+        return helperLength(v, w);
     }
 
     // a common ancestor that participates in shortest ancestral path; -1 if no such path
     public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
+
+        if (v == null || w == null) throw new IllegalArgumentException();
+        for (int vThis: v) if (vThis < 0 || vThis >= dg.V()) throw new IllegalArgumentException();
+        for (int wThis: w) if (wThis < 0 || wThis >= dg.V()) throw new IllegalArgumentException();
+
+        return helperAncestor(v, w);
+
+    }
+
+    private int helperLength(Iterable<Integer> v, Iterable<Integer> w) {
 
         Set<Integer> vSet = new HashSet<Integer>();
         Set<Integer> wSet = new HashSet<Integer>();
@@ -204,9 +66,80 @@ public class SAP {
 
         for (int i: v) {
             vQueue.enqueue(i);
+            vSet.add(i);
         }
         for (int i: w) {
             wQueue.enqueue(i);
+            wSet.add(i);
+        }
+
+        boolean[] vIsVisited = new boolean[dg.V()];
+        boolean[] wIsVisited = new boolean[dg.V()];
+
+        int[] vDistTo = new int[dg.V()];
+        int[] wDistTo = new int[dg.V()];
+
+        while (!vQueue.isEmpty() || !wQueue.isEmpty()) {
+
+            int vCurr = -1, wCurr = -1, vDist = 0, wDist = 0;
+            if (!vQueue.isEmpty()) {
+                vCurr = vQueue.dequeue();
+                vDist = vDistTo[vCurr];
+            }
+            if (!wQueue.isEmpty()) {
+                wCurr = wQueue.dequeue();
+                wDist = wDistTo[wCurr];
+            }
+
+            if (vSet.contains(wCurr)) {
+                return vDistTo[wCurr] + wDistTo[wCurr];
+            } else if (wSet.contains(vCurr)) {
+                return vDistTo[vCurr] + wDistTo[vCurr];
+            }
+
+            if (vCurr != -1) {
+                for (int adj: dg.adj(vCurr)) {
+                    if (!vIsVisited[adj]) {
+                        vSet.add(adj);
+                        vQueue.enqueue(adj);
+                        vIsVisited[adj] = true;
+                        if (vDistTo[adj] < vDist + 1) {
+                            vDistTo[adj] = vDist + 1;
+                        }
+                    }
+                }
+            }
+            if (wCurr != -1) {
+                for (int adj: dg.adj(wCurr)) {
+                    if (!wIsVisited[adj]) {
+                        wSet.add(adj);
+                        wQueue.enqueue(adj);
+                        wIsVisited[adj] = true;
+                        if (wDistTo[adj] < wDist + 1) {
+                            wDistTo[adj] = wDist + 1;
+                        }
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+
+    private int helperAncestor(Iterable<Integer> v, Iterable<Integer> w) {
+
+        Set<Integer> vSet = new HashSet<Integer>();
+        Set<Integer> wSet = new HashSet<Integer>();
+
+        Queue<Integer> vQueue = new Queue<Integer>();
+        Queue<Integer> wQueue = new Queue<Integer>();
+
+        for (int i: v) {
+            vQueue.enqueue(i);
+            vSet.add(i);
+        }
+        for (int i: w) {
+            wQueue.enqueue(i);
+            wSet.add(i);
         }
 
         boolean[] vIsVisited = new boolean[dg.V()];
@@ -243,20 +176,6 @@ public class SAP {
             }
         }
         return -1;
-    }
-
-    // ML added
-    private int getRoot() {
-        boolean flag = false;
-        int root = -1;
-        for (int i = 0; i < dg.V(); i++) {
-            if (dg.outdegree(i) == 0) {
-                if (flag) return -1;
-                flag = true;
-                root = i;
-            }
-        }
-        return root;
     }
 
     // do unit testing of this class
--- src/main/java/week_1/WordNet.java
+++ src/main/java/week_1/WordNet.java
@@ -1,11 +1,14 @@
 package week_1;
 
 import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.DirectedCycle;
 import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.Queue;
-import edu.princeton.cs.algs4.Stack;
 
-import java.util.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Arrays;
 
 /**
  * @author Monali L on 2/8/2020
@@ -13,11 +16,9 @@ import java.util.*;
 
 public class WordNet {
 
-    private Map<Integer, List<String>> idMap;
-    private Map<String, List<Integer>> nounMap;
-    private Digraph dg;
-    private Digraph rdg;
-    private int root;
+    private final Map<Integer, List<String>> idMap;
+    private final Map<String, List<Integer>> nounMap;
+    private final Digraph dg;
 
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms) {
@@ -56,182 +57,55 @@ public class WordNet {
             }
         }
 
-        rdg = dg.reverse();
-
-        if (!isRootedDag()) throw new IllegalArgumentException();
-    }
-
-    private boolean isRootedDag() {
-        boolean flag = false;
-        for (int id: idMap.keySet()) {
-            if (dg.outdegree(id) == 0) {
-                if (flag) return false;
-                flag = true;
-                this.root = id;
-            }
-        }
-        return true;
+        DirectedCycle directedCycle = new DirectedCycle(dg);
+        if (directedCycle.hasCycle()) throw new IllegalArgumentException();
     }
 
     // returns all WordNet nouns
     public Iterable<String> nouns() {
-        List<String> l = new ArrayList<String>();
-        for (int id: idMap.keySet()) {
-            l.addAll(idMap.get(id));
-        }
-        return l;
+        return nounMap.keySet();
     }
 
     // is the word a WordNet noun?
     public boolean isNoun(String word) {
-
         if (word == null) throw new IllegalArgumentException();
-
-        if (idMap.get(root).contains(word)) return true;
-        edu.princeton.cs.algs4.Stack<Integer> stack = new Stack<Integer>();
-        stack.push(root);
-        boolean[] isVisited = new boolean[rdg.V()];
-        isVisited[root-1] = true;
-
-        while(!stack.isEmpty()) {
-            int current = stack.pop();
-            isVisited[current-1] = true;
-            Iterable<Integer> itr = rdg.adj(current);
-            for (int id: itr) {
-                if (idMap.get(id).contains(word)) return true;
-                if (!isVisited[id-1]) {
-                    stack.push(id);
-                }
-            }
-        }
-        return false;
+        return nounMap.containsKey(word);
     }
 
     // distance between nounA and nounB (defined below)
     public int distance(String nounA, String nounB) {
-        //  The methods distance() and sap() should run in time linear in the size of the WordNet digraph
-        // To implement BFS
-        if (nounA == null || nounB == null) throw new IllegalArgumentException();
 
-        Set<Integer> vSet = new HashSet<Integer>();
-        Set<Integer> wSet = new HashSet<Integer>();
+        if (nounA == null || nounB == null || !nounMap.containsKey(nounA) || !nounMap.containsKey(nounB)) throw new IllegalArgumentException();
 
-        Queue<Integer> vQueue = new Queue<Integer>();
-        Queue<Integer> wQueue = new Queue<Integer>();
+        SAP sap = new SAP(dg);
+        List<Integer> vList = new ArrayList<Integer>(nounMap.get(nounA));
+        List<Integer> wList = new ArrayList<Integer>(nounMap.get(nounB));
 
-        for (int n: nounMap.get(nounA)) vQueue.enqueue(n);
-        for (int n: nounMap.get(nounB)) wQueue.enqueue(n);
-
-        boolean[] vIsVisited = new boolean[dg.V()];
-        boolean[] wIsVisited = new boolean[dg.V()];
-
-        int[] vDistTo = new int[dg.V()];
-        int[] wDistTo = new int[dg.V()];
-
-        while (!vQueue.isEmpty() || !wQueue.isEmpty()) {
-
-            int vCurr = -1, wCurr = -1, vDist = 0, wDist = 0;
-            if (!vQueue.isEmpty()) {
-                vCurr = vQueue.dequeue();
-                vDist = vDistTo[vCurr];
-            }
-            if (!wQueue.isEmpty()) {
-                wCurr = wQueue.dequeue();
-                wDist = wDistTo[wCurr];
-            }
-            if (vSet.contains(wCurr)) {
-                return vDistTo[wCurr] + wDistTo[wCurr];
-            } else if (wSet.contains(vCurr)){
-                return vDistTo[vCurr] + wDistTo[vCurr];
-            }
-
-            if (vCurr != -1) {
-                for (int adj: dg.adj(vCurr)) {
-                    if (!vIsVisited[adj]) {
-                        vSet.add(adj);
-                        vQueue.enqueue(adj);
-                        vIsVisited[adj] = true;
-                        if (vDistTo[adj] < vDist + 1) {
-                            vDistTo[adj] = vDist + 1;
-                        }
-                    }
-                }
-            }
-
-            if (wCurr != -1) {
-                for (int adj: dg.adj(wCurr)) {
-                    if (!wIsVisited[adj]) {
-                        wSet.add(adj);
-                        wQueue.enqueue(adj);
-                        wIsVisited[adj] = true;
-                        if (wDistTo[adj] < wDist + 1) {
-                            wDistTo[adj] = wDist + 1;
-                        }
-                    }
-                }
-            }
-        }
-        return -1;
+        return sap.length(vList, wList);
     }
 
     // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
     // in a shortest ancestral path (defined below)
     public String sap(String nounA, String nounB) {
 
-        if (nounA == null || nounB == null) throw new IllegalArgumentException();
+        if (nounA == null || nounB == null || !nounMap.containsKey(nounA) || !nounMap.containsKey(nounB)) throw new IllegalArgumentException();
 
-        Set<Integer> vSet = new HashSet<Integer>();
-        Set<Integer> wSet = new HashSet<Integer>();
+        SAP sap = new SAP(dg);
+        List<Integer> vList = new ArrayList<Integer>(nounMap.get(nounA));
+        List<Integer> wList = new ArrayList<Integer>(nounMap.get(nounB));
 
-        Queue<Integer> vQueue = new Queue<Integer>();
-        Queue<Integer> wQueue = new Queue<Integer>();
+        StringBuilder rslt = new StringBuilder();
+        int rsltId = sap.ancestor(vList, wList);
+        for (String s: idMap.get(rsltId)) rslt.append(" ").append(s);
 
-        for (int n: nounMap.get(nounA)) vQueue.enqueue(n);
-        for (int n: nounMap.get(nounB)) wQueue.enqueue(n);
-
-        boolean[] vIsVisited = new boolean[dg.V()];
-        boolean[] wIsVisited = new boolean[dg.V()];
-
-        while (!vQueue.isEmpty() || !wQueue.isEmpty()) {
-
-            int vCurr = vQueue.isEmpty() ? -1 : vQueue.dequeue();
-            int wCurr = wQueue.isEmpty() ? -1 : wQueue.dequeue();
-
-            StringBuilder rslt = new StringBuilder();
-            if (vSet.contains(wCurr)) {
-                for (String s: idMap.get(wCurr)) rslt.append(" ").append(s);
-                return rslt.toString().trim();
-            } else if (wSet.contains(vCurr)) {
-                for (String s: idMap.get(vCurr)) rslt.append(" ").append(s);
-                return rslt.toString().trim();
-            }
-
-            if (vCurr != -1) {
-                for (int adj: dg.adj(vCurr)) {
-                    if (!vIsVisited[adj]) {
-                        vSet.add(adj);
-                        vQueue.enqueue(adj);
-                        vIsVisited[adj] = true;
-                    }
-                }
-            }
-            if (wCurr != -1) {
-                for (int adj: dg.adj(wCurr)) {
-                    if (!wIsVisited[adj]) {
-                        wSet.add(adj);
-                        wQueue.enqueue(adj);
-                        wIsVisited[adj] = true;
-                    }
-                }
-            }
-        }
-        return null;
+        return rslt.toString().trim();
     }
 
     // do unit testing of this class
     public static void main(String[] args) {
         String folderPath = "C:\\Users\\monal\\IdeaProjects\\Coursera\\src\\main\\resources\\";
         WordNet wordNet = new WordNet(folderPath + "synsets.txt", folderPath + "hypernyms.txt");
+        wordNet.distance("demotion", "variation");
     }
 
 }
