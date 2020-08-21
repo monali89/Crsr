7a636a760e35c18d2847f81e3d67315279947b22
Monali Lakhote <monalidl@gmail.com>
2020-08-21 14:00:28 -0400

    Cleanup

--- src/main/java/week_4/BoggleSolver.java
+++ src/main/java/week_4/BoggleSolver.java
@@ -18,10 +18,8 @@ public class BoggleSolver {
         Trie left, mid, right;
     }
 
-    private final Trie root;
     private final Trie[] trieArray;
     private List<String> validWords;
-    private List<String> validWords2;
 
     // Initializes the data structure using the given array of strings as the dictionary.
     // (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
@@ -31,39 +29,9 @@ public class BoggleSolver {
         for (int i = 0; i < dictionary.length; i++) {
             String w = dictionary[i];
             if (w.length() < 3) continue;
-            //int idx = (w.charAt(0)-'A')*(w.charAt(1)-'A') + 26;
             int idx = (w.charAt(0)-'A')*26 + (w.charAt(1)-'A');
             trieArray[idx] = put(trieArray[idx], w, i+1, 2);
         }
-
-        /*for (int i = 0; i < trieArray.length; i++) {
-            System.out.print((trieArray[i] != null ? trieArray[i].c : "NULL") + " ");
-        }
-        System.out.println();
-
-        for (String s : dictionary) {
-            if (s.length() < 3) continue;
-            //int idx = (s.charAt(0) - 'A') * (s.charAt(1) - 'A') + 26;
-            int idx = (s.charAt(0)-'A')*26 + (s.charAt(1)-'A');
-            Trie t = get(trieArray[idx], s, 2);
-            if ((s.charAt(0) + "" + s.charAt(1)).equals("TW"))
-            // if (s.equals("ATEYE"))
-                System.out.print(s + " - " + (t != null ? t.val : "NULL") + " | ");
-        }
-        System.out.println();*/
-
-        Trie root1;
-        root1 = new Trie();
-        for (int i = 0; i < dictionary.length; i++) {
-            root1 = put(root1, dictionary[i], i+1, 0);
-        }
-        root = root1;
-
-        /*for (String s : dictionary) {
-            Trie t = get(root1, s, 0);
-            System.out.print(s + " - " + (t != null ? t.val : "NULL") + " | ");
-        }*/
-
     }
 
     private Trie put(Trie x, String key, int val, int d) {
@@ -89,29 +57,12 @@ public class BoggleSolver {
         else return x;
     }
 
-    /*private Iterable<String> keysWithPrefix(String prefix) {
-        Queue<String> queue = new Queue<String>();
-        Node x = get(root, prefix, 0);
-        collect(x, prefix, queue);
-        return queue;
-    }
-
-    private void collect(Node x, String prefix, Queue<String> q)
-    {
-        if (x == null) return;
-        if (x.val > 0) q.enqueue(prefix);
-        collect(x.left, prefix + x.c, q);
-        collect(x.mid, prefix + x.c, q);
-        collect(x.right, prefix + x.c, q);
-    }*/
-
     // Returns the set of all valid words in the given Boggle board, as an Iterable.
     public Iterable<String> getAllValidWords(BoggleBoard board) {
 
         long startTime = System.currentTimeMillis();
 
         validWords = new ArrayList<String>();
-        validWords2 = new ArrayList<String>();
         boolean[][] isVisited = new boolean[board.rows()][board.cols()];
         StringBuilder str = new StringBuilder();
         for (int i = 0; i < board.rows(); i++) {
@@ -134,15 +85,9 @@ public class BoggleSolver {
         if (str.length() >= 3) {
             int idx = (str.charAt(0)-'A')*26 + (str.charAt(1)-'A');
             Trie t = get(trieArray[idx], str.toString(), 2);
-            //Trie t2 = get(root, str.toString(), 0);
             if (t != null && t.val > 0 && !validWords.contains(str.toString())) {
-                //System.out.println("DEBUG: " + str + " | " + t.c + " | " + t.val);
                 validWords.add(str.toString());
             }
-            /* else if (t2 != null && t2.val > 0 && !validWords2.contains(str.toString())) {
-                validWords2.add(str.toString());
-                System.out.println("Found in other version - " + str);
-            }*/
          }
         int[] rows = {-1, -1, -1, 0, 0, 0, 1, 1, 1};
         int[] cols = {-1, 0, 1, -1, 0, 1, -1, 0, 1};
@@ -173,11 +118,10 @@ public class BoggleSolver {
         String folderPath = "C:\\Users\\monal\\IdeaProjects\\Coursera\\src\\main\\resources\\week_4\\";
         args = new String[2];
         args[0] = folderPath + "dictionary-yawl.txt";
-        args[1] = folderPath + "board-points300.txt";
+        args[1] = folderPath + "board-points4540.txt";
 
         In in = new In(args[0]);
         String[] dictionary = in.readAllStrings();
-        //String[] dictionary = {"PAGURIAN", "PAID", "PAIDEUTIC"};
         BoggleSolver solver = new BoggleSolver(dictionary);
         BoggleBoard board = new BoggleBoard(args[1]);
         int score = 0;
