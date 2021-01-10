20d07867e00602dd6452a4e512f93d0600537d6c
Monali Lakhote <monalidl@gmail.com>
2021-01-10 13:30:45 -0500

    getting path by bfs

--- src/main/java/week_3/Edmonds_Karp_Impl.java
+++ src/main/java/week_3/Edmonds_Karp_Impl.java
@@ -1,12 +1,7 @@
 package week_3;
 
-import edu.princeton.cs.algs4.DirectedEdge;
 import edu.princeton.cs.algs4.EdgeWeightedDigraph;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import edu.princeton.cs.algs4.Queue;
 
 /**
  * @author Monali L on 12/28/2020
@@ -14,58 +9,82 @@ import java.util.Map;
 
 public class Edmonds_Karp_Impl {
 
-    public static void main(String[] args) {
+    int[] vertices;
+    int[][] capacity;
+    int[][] flow;
+    int source;
+    int sink;
 
-        EdgeWeightedDigraph dg = new EdgeWeightedDigraph(8);
+    Edmonds_Karp_Impl(int size, int[][] weights, int source, int sink) {
 
-        dg.addEdge(new DirectedEdge(0, 1, 10));
-        dg.addEdge(new DirectedEdge(0, 2, 5));
-        dg.addEdge(new DirectedEdge(0, 3, 15));
+        if (weights.length == 0 || weights[0].length != size) throw new IllegalArgumentException();
 
-        dg.addEdge(new DirectedEdge(1, 4, 9));
-        dg.addEdge(new DirectedEdge(1, 5, 15));
-        dg.addEdge(new DirectedEdge(1, 2, 4));
+        vertices = new int[size];
+        capacity = new int[size][size];
+        flow = new int[size][size];
+        this.source = source;
+        this.sink = sink;
 
-        dg.addEdge(new DirectedEdge(2, 5, 8));
-        dg.addEdge(new DirectedEdge(2, 3, 4));
-
-        dg.addEdge(new DirectedEdge(3, 6, 16));
-
-        dg.addEdge(new DirectedEdge(4, 7, 10));
-        dg.addEdge(new DirectedEdge(4, 5, 15));
-
-        dg.addEdge(new DirectedEdge(5, 7,10));
-        dg.addEdge(new DirectedEdge(5, 6,15));
-
-        dg.addEdge(new DirectedEdge(6, 2,6));
-        dg.addEdge(new DirectedEdge(6, 7, 10));
-
-        System.out.println("Max Flow: " + GetMaxFlow(dg, 0, 7));
+        for (int i = 0; i < size; i++) {
+            vertices[i] = i;
+            for (int j = 0; j < size; j++) {
+                capacity[i][j] = weights[i][j];
+                flow[i][j] = 0;
+            }
+        }
     }
 
     public static int GetMaxFlow(EdgeWeightedDigraph g, int source, int sink){
 
         int maxFlow = 0;
-        List<DirectedEdge> parent = new ArrayList<DirectedEdge>();
-        Map<DirectedEdge, Integer> flow = new HashMap<DirectedEdge, Integer>();
-
-        // this will true until path doesn't exist
-        while (bfs(g, source, sink, parent)) {
-
-            // Calculate current path's (returned by bfs in parent) value
-            int s = sink;
-            int currentFlow = 0;
-            while (s != source) {
-                //currentFlow = Math.min();
-
-            }
-
-        }
 
         return maxFlow;
     }
 
-    public static boolean bfs(EdgeWeightedDigraph g, int s, int t, List<DirectedEdge> parent){
-        return false;
+    public int[] bfs(){
+
+        int[] pred = new int[vertices.length];
+        Queue<Integer> queue = new Queue<Integer>();
+        boolean[] isVisited = new boolean[vertices.length];
+        queue.enqueue(source);
+        isVisited[source] = true;
+
+        while (!queue.isEmpty()) {
+            int v = queue.dequeue();
+            for (int i = 0; i < capacity[v].length; i++) {
+                if (capacity[v][i] == 0) continue;
+                if (pred[i] == 0 && capacity[v][i] > flow[v][i]) {
+                    pred[i] = v;
+                    queue.enqueue(i);
+                }
+            }
+        }
+
+        int s = sink;
+        System.out.print(s);
+        while (s != source) {
+            System.out.print(" " + pred[s]);
+            s = pred[s];
+        }
+        System.out.println();
+
+        return pred;
+    }
+
+    public static void main(String[] args) {
+
+        int[][] weights = {
+                {0, 10,  5, 15,  0,  0,  0,  0},
+                {0,  0,  4,  0,  9,  0, 15,  0},
+                {0,  0,  0,  4,  8,  0,  0,  0},
+                {0,  0,  0,  0,  0,  0, 16,  0},
+                {0,  0,  0,  0,  0, 15,  0, 10},
+                {0,  0,  0,  0,  0,  0,  0, 10},
+                {0,  0,  0,  0,  0,  0, 15, 10},
+                {0,  0,  0,  0,  0,  0,  0,  0}
+        };
+
+        Edmonds_Karp_Impl obj = new Edmonds_Karp_Impl(8, weights, 0, 7);
+        int[] bfsPath = obj.bfs();
     }
 }
