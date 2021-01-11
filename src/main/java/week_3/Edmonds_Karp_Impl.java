43b6cbb27d00c36bd5898100fb0057db4d37e82d
Monali Lakhote <monalidl@gmail.com>
2021-01-11 17:12:44 -0500

    algo implementation working

--- src/main/java/week_3/Edmonds_Karp_Impl.java
+++ src/main/java/week_3/Edmonds_Karp_Impl.java
@@ -1,7 +1,8 @@
 package week_3;
 
-import edu.princeton.cs.algs4.EdgeWeightedDigraph;
 import edu.princeton.cs.algs4.Queue;
+
+import java.util.Arrays;
 
 /**
  * @author Monali L on 12/28/2020
@@ -28,15 +29,40 @@ public class Edmonds_Karp_Impl {
         for (int i = 0; i < size; i++) {
             vertices[i] = i;
             for (int j = 0; j < size; j++) {
-                capacity[i][j] = weights[i][j];
+                capacity[i][j] = weights[i][j] == 0 ? -1 : weights[i][j];
                 flow[i][j] = 0;
             }
         }
     }
 
-    public static int GetMaxFlow(EdgeWeightedDigraph g, int source, int sink){
+    public int GetMaxFlow(){
 
         int maxFlow = 0;
+        int[] parent = bfs();
+
+        while (parent[sink] != -1) {
+
+            int pathFlow = Integer.MAX_VALUE;
+
+            // Calculate the minimum flow that can be sent on this path
+            for (int startVertex = parent[sink], endVertex = sink;
+                 startVertex != -1;
+                 endVertex = startVertex, startVertex = parent[endVertex]) {
+                pathFlow = Math.min(pathFlow, capacity[startVertex][endVertex] - flow[startVertex][endVertex]);
+            }
+
+            // Add current min path flow to overall flow
+            maxFlow += pathFlow;
+
+            // Update flow by the minimum capacity
+            for (int startVertex = parent[sink], endVertex = sink;
+                 startVertex != -1;
+                 endVertex = startVertex, startVertex = parent[endVertex]) {
+                flow[startVertex][endVertex] += pathFlow;
+                flow[endVertex][startVertex] -= pathFlow;
+            }
+            parent = bfs();
+        }
 
         return maxFlow;
     }
@@ -44,30 +70,21 @@ public class Edmonds_Karp_Impl {
     public int[] bfs(){
 
         int[] pred = new int[vertices.length];
+        Arrays.fill(pred, -1);
+
         Queue<Integer> queue = new Queue<Integer>();
-        boolean[] isVisited = new boolean[vertices.length];
         queue.enqueue(source);
-        isVisited[source] = true;
 
         while (!queue.isEmpty()) {
             int v = queue.dequeue();
             for (int i = 0; i < capacity[v].length; i++) {
-                if (capacity[v][i] == 0) continue;
-                if (pred[i] == 0 && capacity[v][i] > flow[v][i]) {
+                if (capacity[v][i] == -1) continue;
+                if (pred[i] == -1 && capacity[v][i] > flow[v][i]) {
                     pred[i] = v;
                     queue.enqueue(i);
                 }
             }
         }
-
-        int s = sink;
-        System.out.print(s);
-        while (s != source) {
-            System.out.print(" " + pred[s]);
-            s = pred[s];
-        }
-        System.out.println();
-
         return pred;
     }
 
@@ -85,6 +102,7 @@ public class Edmonds_Karp_Impl {
         };
 
         Edmonds_Karp_Impl obj = new Edmonds_Karp_Impl(8, weights, 0, 7);
-        int[] bfsPath = obj.bfs();
+
+        System.out.println("Max flow: " + obj.GetMaxFlow());
     }
 }
