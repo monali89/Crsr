3423b5f11f212f579881fb6e27688f02061cdc30
Monali Lakhote <monalidl@gmail.com>
2021-02-03 11:48:50 -0500

    cleanup, updates to elimination certificate

--- src/main/java/week_3/BaseballElimination.java
+++ src/main/java/week_3/BaseballElimination.java
@@ -21,6 +21,7 @@ public class BaseballElimination {
     int[] r;   // i's total remaining games
     int[][] g; // i's games left to play against team j
     Map<String, Integer> teams;
+    Map<Integer, String> reverseTeams;
 
     // create a baseball division from given filename in format specified below
     public BaseballElimination(String filename) {
@@ -32,11 +33,13 @@ public class BaseballElimination {
             r = new int[lines];
             g = new int[lines][lines];
             teams = new HashMap<String, Integer>();
+            reverseTeams = new HashMap<Integer, String>();
 
             for (int i = 0; i < lines; i++) {
                 String line = bfr.readLine().replaceAll(" +", " ");
                 String[] arr = line.split(" ");
                 teams.put(arr[0], i);
+                reverseTeams.put(i, arr[0]);
                 w[i] = Integer.parseInt(arr[1]);
                 l[i] = Integer.parseInt(arr[2]);
                 r[i] = Integer.parseInt(arr[3]);
@@ -50,19 +53,6 @@ public class BaseballElimination {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    // Get the team with the least possible wins (current wins + remaining games)
-    private int getLowestTeam() {
-        int minGames = w[0] + r[0];
-        int minTeam = 0;
-        for (int i = 1; i < teams.size(); i++) {
-            if (minGames < (w[i] + r[i])) {
-                minTeam = i;
-                minGames = w[i] + r[i];
-            }
-        }
-        return minTeam;
     }
 
     // number of teams
@@ -117,96 +107,9 @@ public class BaseballElimination {
         System.out.println("Nontrivial Elimination");
 
         // create a flow network without given team
+        CreateGraph(team);
 
-        // Graph representation
-        int[] vertices;
-        int[][] capacity;
-        int[][] flow;
-        int source;
-        int sink;
-
-        // Mapping for graph integer index to actual vertex
-        // Map<Integer, String> indexMap;
-        Map<String, Integer> indexMap;
-
-        // Converting input into graph representation
-
-        // START
-
-        // total graph vertices = source + total game vertices + team vertices + sink
-        int size = 1 + getComb(teams.size()-1) + (teams.size()-1) + 1;
-
-        source = 0;
-        sink = size - 1;
-
-        // create a mapping to refer to actual vertex from graph's integer vertex
-        vertices = new int[size];
-        for (int i = 0; i < size; i++) {
-            if (i == teams.get(team)) continue;
-            vertices[i] = i;
-        }
-
-        //indexMap = new HashMap<Integer, String>();
-        indexMap = new HashMap<String, Integer>();
-        int graphIndex = source;
-        indexMap.put("source", graphIndex++); // source vertex
-
-        for (int i = 0; i < g.length; i++) { // game vertices
-            if (i == teams.get(team)) continue;
-            for (int j = i+1; j < g[i].length; j++) {
-                if (j == teams.get(team)) continue;
-                indexMap.put(i + "-" + j, graphIndex++);
-            }
-        }
-
-        for (int i = 0; i < g.length; i++) { // team vertices
-            if (i == teams.get(team)) continue;
-            indexMap.put(String.valueOf(i), graphIndex++);
-        }
-
-        indexMap.put("sink", graphIndex); // sink vertex
-
-        // Initialize capacities
-
-        capacity = new int[size][size];
-        for (int i = 0; i < size; i++) {
-            Arrays.fill(capacity[i], -1);
-        }
-
-        graphIndex = 1; // capacities from source to game vertices
-        for (int i = 0; i < g.length; i++) {
-            if (i == teams.get(team)) continue;
-            for (int j = i+1; j < g.length; j++) {
-                if (j == teams.get(team)) continue;
-                capacity[source][graphIndex++] = g[i][j];
-            }
-        }
-
-        for (int i = 0; i < g.length; i++) { // capacities from game vertices to team vertices
-            if (i == teams.get(team)) continue;
-            for (int j = i+1; j < g[i].length; j++) {
-                if (j == teams.get(team)) continue;
-                capacity[indexMap.get(i + "-" + j)][indexMap.get(String.valueOf(i))] = Integer.MAX_VALUE;
-                capacity[indexMap.get(i + "-" + j)][indexMap.get(String.valueOf(j))] = Integer.MAX_VALUE;
-            }
-        }
-
-        for (int i = 0; i < g.length; i++) { // capacities from team vertices to sink
-            if (i == teams.get(team)) continue;
-            int teamIndex = indexMap.get(String.valueOf(i));
-            capacity[teamIndex][sink] = w[teams.get(team)] + r[teams.get(team)] - w[i];
-            //capacity[teamIndex][sink] = w[i] + r[i] - w[getLowestTeam()];
-        }
-
-        flow = new int[size][size]; // initialize remaining flow to be -1/null
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                flow[i][j] = 0;
-            }
-        }
-
-        // END
-        int maxFlow = edk(vertices, capacity, flow, source, sink);
+        int maxFlow = edk(capacity, flow, source, sink);
 
         if (maxFlow == 0) return false;
 
@@ -217,16 +120,93 @@ public class BaseballElimination {
             if (i == teams.get(team)) continue;
             for (int j = i+1; j < g.length; j++) {
                 if (j == teams.get(team)) continue;
-                if (capacity[source][indexMap.get(i + "-" + j)] != flow[source][indexMap.get(i + "-" + j)]) {
+                if (capacity[source][teamToGraphIndex.get(i + "-" + j)] != flow[source][teamToGraphIndex.get(i + "-" + j)]) {
                     eliminationCertTeams.add(i + "-" + j);
                 }
             }
         }
 
-        getMinCut(vertices, capacity, flow, source, sink);
+        getMinCut(capacity, flow, source, sink);
 
         return eliminationCertTeams.size() > 0;
         //return false;
+    }
+
+    // Graph representation
+    //int[] vertices;
+    int[][] capacity;
+    int[][] flow;
+    int source;
+    int sink;
+    // Map<Integer, String> indexMap;
+    Map<String, Integer> teamToGraphIndex; // Mapping for graph integer index to actual vertex
+    Map<Integer, String> graphToTeamIndex; // Mapping from graph int index to team representation
+
+    private void CreateGraph(String team) {
+
+        // total graph vertices = source + total game vertices + team vertices + sink
+        int size = 1 + getComb(teams.size()-1) + (teams.size()-1) + 1;
+        source = 0;
+        sink = size - 1;
+
+        teamToGraphIndex = new HashMap<String, Integer>();
+        graphToTeamIndex = new HashMap<Integer, String>();
+
+        teamToGraphIndex.put("source", source);
+        teamToGraphIndex.put("sink", sink);
+        graphToTeamIndex.put(source, "source");
+        graphToTeamIndex.put(sink, "sink");
+
+        int graphIndex = source+1;
+
+        for (int i = 0; i < g.length; i++) { // game vertices
+            if (i == teams.get(team)) continue;
+            for (int j = i+1; j < g[i].length; j++) {
+                if (j == teams.get(team)) continue;
+                teamToGraphIndex.put(i + "-" + j, graphIndex);
+                graphToTeamIndex.put(graphIndex, i + "-" + j);
+                graphIndex++;
+            }
+        }
+
+        for (int i = 0; i < g.length; i++) { // team vertices
+            if (i == teams.get(team)) continue;
+            teamToGraphIndex.put(String.valueOf(i), graphIndex);
+            graphToTeamIndex.put(graphIndex, String.valueOf(i));
+            graphIndex++;
+        }
+
+        // Initialize capacities
+        capacity = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            Arrays.fill(capacity[i], -1);
+        }
+
+        graphIndex = 1;
+
+        for (int i = 0; i < g.length; i++) {
+            if (i == teams.get(team)) continue; // skip if given team node
+            for (int j = i+1; j < g.length; j++) {
+                if (j == teams.get(team)) continue; // skip if given team node
+
+                capacity[source][graphIndex++] = g[i][j]; // from source to game vertex
+
+                // from game vertex to team vertex
+                capacity[teamToGraphIndex.get(i + "-" + j)][teamToGraphIndex.get(String.valueOf(i))] = Integer.MAX_VALUE;
+                capacity[teamToGraphIndex.get(i + "-" + j)][teamToGraphIndex.get(String.valueOf(j))] = Integer.MAX_VALUE;
+            }
+
+            // from team vertex to sink
+            int teamIndex = teamToGraphIndex.get(String.valueOf(i));
+            capacity[teamIndex][sink] = w[teams.get(team)] + r[teams.get(team)] - w[i];
+        }
+
+        flow = new int[size][size]; // initialize remaining flow to be -1/null
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                flow[i][j] = 0;
+            }
+        }
     }
 
     private int getComb(int n) {
@@ -241,29 +221,11 @@ public class BaseballElimination {
         return fact;
     }
 
-    private int edk(int[] vertices, int[][] capacity, int[][] flow, int source, int sink) {
+    private int edk(int[][] capacity, int[][] flow, int source, int sink) {
         int maxFlow = 0;
-        int[] parent = bfs(vertices, capacity, flow, source, sink);
+        int[] parent = bfs( capacity, flow, source, sink);
 
         while (parent[sink] != -1) {
-
-            // temp prints
-            /*System.out.print("Flowing from  : ");
-            System.out.print(sink);
-            for (int startVertex = parent[sink], endVertex = sink;
-                 startVertex != -1;
-                 endVertex = startVertex, startVertex = parent[endVertex]) {
-                System.out.print(" <- " + startVertex);
-            }
-            System.out.println();
-            System.out.print("Current Flow  : ");
-            for (int startVertex = parent[sink], endVertex = sink;
-                 startVertex != -1;
-                 endVertex = startVertex, startVertex = parent[endVertex]) {
-                System.out.print(flow[startVertex][endVertex] + " | ");
-            }
-            System.out.println();*/
-
             int pathFlow = Integer.MAX_VALUE;
 
             // Calculate the minimum flow that can be sent on this path
@@ -284,26 +246,13 @@ public class BaseballElimination {
                 flow[endVertex][startVertex] -= pathFlow;
             }
 
-            //System.out.println("PathFlow: " + pathFlow);
-
-            // temp prints
-            /*System.out.print("Updated Flow  : ");
-            for (int startVertex = parent[sink], endVertex = sink;
-                 startVertex != -1;
-                 endVertex = startVertex, startVertex = parent[endVertex]) {
-                System.out.print(flow[startVertex][endVertex] + " | ");
-            }
-            System.out.println();
-            System.out.println();*/
-
-            parent = bfs(vertices, capacity, flow, source, sink);
+            parent = bfs(capacity, flow, source, sink);
         }
-
         return maxFlow;
     }
 
-    private int[] bfs(int[] vertices, int[][] capacity, int[][] flow, int source, int sink) {
-        int[] pred = new int[vertices.length];
+    private int[] bfs(int[][] capacity, int[][] flow, int source, int sink) {
+        int[] pred = new int[capacity.length];
         Arrays.fill(pred, -1);
 
         Queue<Integer> queue = new Queue<Integer>();
@@ -327,10 +276,9 @@ public class BaseballElimination {
         return eliminationCertTeams;
     }
 
-    private void getMinCut(int[] vertices, int[][] capacity, int[][] flow, int source, int sink) {
+    private void getMinCut(int[][] capacity, int[][] flow, int source, int sink) {
 
-        int[][] residual = new int[vertices.length][vertices.length];
-        List<Integer> temp = new ArrayList<Integer>();
+        int[][] residual = new int[capacity.length][capacity.length];
 
         for (int i = 0; i < residual.length; i++) {
             for (int j = 0; j < residual.length; j++) {
@@ -338,10 +286,8 @@ public class BaseballElimination {
             }
         }
 
-        //int[] pred = new int[vertices.length];
-        boolean[] isVisited = new boolean[vertices.length];
-        //Arrays.fill(pred, -1);
-
+        Set<String> certificateOfEliminationTeams = new HashSet<String>();
+        boolean[] isVisited = new boolean[capacity.length];
         Stack<Integer> stack = new Stack<Integer>();
         stack.push(source);
         isVisited[source] = true;
@@ -351,55 +297,21 @@ public class BaseballElimination {
             for (int i = 0; i < residual.length; i++) {
                 if (residual[v][i] > 0 && !isVisited[i]) {
                     stack.push(i);
-                    temp.add(i);
-                    //pred[i] = v;
+                    String[] tempTeams = graphToTeamIndex.get(i).split("-");
+                    for (int j = 0; j < tempTeams.length; j++) {
+                        certificateOfEliminationTeams.add(reverseTeams.get(Integer.parseInt(tempTeams[j])));
+                    }
                     isVisited[i] = true;
                 }
             }
         }
-
-        /*Queue<Integer> queue = new Queue<Integer>();
-        queue.enqueue(source);
-
-        while (!queue.isEmpty()) {
-            int v = queue.dequeue();
-            for (int i = 0; i < capacity[v].length; i++) {
-                if (residual[v][i] == -1) continue;
-                if (pred[i] == -1 && residual[v][i] > 0) {
-                    pred[i] = v;
-                    queue.enqueue(i);
-                }
-            }
-        }*/
-
-        System.out.println("Residual Graph");
-        for (int i = 0; i < capacity.length; i++) {
-            for (int j = 0; j < capacity.length; j++) {
-                System.out.print(residual[i][j] + " ");
-            }
-            System.out.println();
-        }
-
-        /*System.out.println("BFS on residual graph");
-        for (int i = 0; i < pred.length; i++) {
-            System.out.print(pred[i] + " ");
-        }
-        System.out.println();*/
-
-        /*for (int startVertex = pred[sink], endVertex = sink;
-             startVertex != -1;
-             endVertex = startVertex, startVertex = pred[endVertex]) {
-            temp.add(startVertex);
-        }*/
-
-        System.out.println("certificateOfElimination - " + temp);
-
+        System.out.println("certificateOfElimination - " + certificateOfEliminationTeams);
     }
 
 
     public static void main(String[] args) {
 
-        String file = "C:\\Users\\monal\\IdeaProjects\\Coursera\\src\\main\\resources\\week_3\\teams4.txt";
+        String file = "C:\\Users\\monal\\IdeaProjects\\Coursera\\src\\main\\resources\\week_3\\teams5.txt";
 
         //BaseballElimination division = new BaseballElimination(args[0]);
         BaseballElimination division = new BaseballElimination(file);
