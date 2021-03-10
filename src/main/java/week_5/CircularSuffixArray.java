761568f3bbbc820b09fda4901a1d846f89ee20c3
Monali Lakhote <monalidl@gmail.com>
2021-03-09 20:25:15 -0500

    week 5 files

---
+++ src/main/java/week_5/CircularSuffixArray.java
@@ -0,0 +1,18 @@
+package week_5;
+
+/**
+ * @author Monali L on 3/9/2021
+ */
+public class CircularSuffixArray {
+    // circular suffix array of s
+    public CircularSuffixArray(String s) {}
+
+    // length of s
+    public int length() {return -1;}
+
+    // returns index of ith sorted suffix
+    public int index(int i) {return -1;}
+
+    // unit testing (required)
+    public static void main(String[] args) {}
+}
