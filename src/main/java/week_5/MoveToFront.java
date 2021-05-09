fe534013be8aad6f1381fb186ffa210015871f53
Monali Lakhote <monalidl@gmail.com>
2021-05-09 14:59:42 -0400

    corrections

--- src/main/java/week_5/MoveToFront.java
+++ src/main/java/week_5/MoveToFront.java
@@ -1,6 +1,7 @@
 package week_5;
 
 import edu.princeton.cs.algs4.BinaryStdIn;
+import edu.princeton.cs.algs4.BinaryStdOut;
 
 /**
  * @author Monali L on 3/9/2021
@@ -8,39 +9,28 @@ import edu.princeton.cs.algs4.BinaryStdIn;
 
 public class MoveToFront {
 
-    private static char[] mtf;
-
     // apply move-to-front encoding, reading from standard input and writing to standard output
     public static void encode() {
-
-        System.out.println("In encode");
-        // mtf = new char[]{'A', 'B', 'C', 'D', 'E', 'F'};
-        mtf = new char[26];
-        for (int i = 0; i < 26; i++) {
-            mtf[i] = (char) ('A' + i);
+        char[] mtf = new char[256];
+        for (int i = 0; i < mtf.length; i++) {
+            mtf[i] = (char) i;
         }
 
         while (!BinaryStdIn.isEmpty()) {
-            char c = BinaryStdIn.readChar();
+            char c = BinaryStdIn.readChar(8);
 
             // find char in mtf
             int idx = 0;
-            while (mtf[idx] != c) {
-                idx++;
-            }
-
-            System.out.print(idx + " ");
+            while (idx < mtf.length && mtf[idx] != c) idx++;
+            // System.out.print(idx + " ");
 
             // push char to front and left rotate array
-            for (int i = idx; i > 0; i--) {
-                mtf[i] = mtf[i-1];
-            }
+            for (int i = idx; i > 0; i--) mtf[i] = mtf[i-1];
             mtf[0] = c;
 
-            // BinaryStdOut.write(idx);
-
+            BinaryStdOut.write(idx, 8);
         }
-        // BinaryStdOut.close();
+        BinaryStdOut.close();
     }
 
     // apply move-to-front decoding, reading from standard input and writing to standard output
