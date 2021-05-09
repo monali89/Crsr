b49ce1491a2f6aaa054d7c5ece6b0ecb3a423184
Monali Lakhote <monalidl@gmail.com>
2021-05-09 16:14:31 -0400

    decode first pass

--- src/main/java/week_5/MoveToFront.java
+++ src/main/java/week_5/MoveToFront.java
@@ -35,7 +35,24 @@ public class MoveToFront {
 
     // apply move-to-front decoding, reading from standard input and writing to standard output
     public static void decode() {
-        System.out.println("In decode");
+        System.out.println("Decode");
+        char[] mtf = new char[256];
+        for (int i = 0; i < mtf.length; i++) {
+            mtf[i] = (char) i;
+        }
+
+        while (!BinaryStdIn.isEmpty()) {
+            int idx = BinaryStdIn.readChar(8);
+            char c = mtf[idx];
+            BinaryStdOut.write(mtf[idx], 8);
+            // System.out.println(idx + " "  + c);
+
+            // move this char to the front
+            for (int i = c; i > 0; i--) mtf[i] = mtf[i-1];
+            mtf[0] = c;
+        }
+        BinaryStdOut.close();
+
     }
 
     // if args[0] is "-", apply move-to-front encoding
